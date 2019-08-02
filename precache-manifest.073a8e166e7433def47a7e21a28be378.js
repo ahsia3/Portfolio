84ff1a75967f9d28fc1069e03efbf3c12b9d42b5
ahsia3 <tonyhsia22@gmail.com>
2019-08-01 21:40:04 -0400

    Updates

---
+++ precache-manifest.073a8e166e7433def47a7e21a28be378.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290d041e1ddb312a79cbea35b17b89d1",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "76b90188e70aff41ca6a",
+    "url": "/React-portfolio/static/css/2.12c9eb85.chunk.css"
+  },
+  {
+    "revision": "a29558ee7c28ddac4654",
+    "url": "/React-portfolio/static/css/main.36bb2287.chunk.css"
+  },
+  {
+    "revision": "76b90188e70aff41ca6a",
+    "url": "/React-portfolio/static/js/2.87fe4b1c.chunk.js"
+  },
+  {
+    "revision": "a29558ee7c28ddac4654",
+    "url": "/React-portfolio/static/js/main.68b14d90.chunk.js"
+  },
+  {
+    "revision": "ee0a708000d28b74b350",
+    "url": "/React-portfolio/static/js/runtime~main.ebbf391c.js"
+  },
+  {
+    "revision": "e07c8099bb1070431edb391eed2ef2e8",
+    "url": "/React-portfolio/static/media/AHpic.e07c8099.png"
+  },
+  {
+    "revision": "dee9dd82ab75daa7ef39f4142f890d7a",
+    "url": "/React-portfolio/static/media/ForgeClicker.dee9dd82.PNG"
+  },
+  {
+    "revision": "f127d0b22f692b1eb7c032712756dab2",
+    "url": "/React-portfolio/static/media/MazeRunner_2.f127d0b2.PNG"
+  },
+  {
+    "revision": "23ef365189b4c76e78c6f1fd3f8b732a",
+    "url": "/React-portfolio/static/media/SecureSay.23ef3651.PNG"
+  },
+  {
+    "revision": "75e185076ce64838cbb2a1a70d2a43f7",
+    "url": "/React-portfolio/static/media/SuperSurvivor.75e18507.PNG"
+  },
+  {
+    "revision": "fa500a779ad1df2b9b4b923c1dd76569",
+    "url": "/React-portfolio/static/media/TheLostCore.fa500a77.PNG"
+  },
+  {
+    "revision": "c9b13621c45054604db472f3cc01eb63",
+    "url": "/React-portfolio/static/media/WASDodge.c9b13621.PNG"
+  },
+  {
+    "revision": "63338bbc09ddb7452697a59c7ee81859",
+    "url": "/React-portfolio/static/media/chainVote.63338bbc.PNG"
+  },
+  {
+    "revision": "de668f7144d44286edf6150100e13f83",
+    "url": "/React-portfolio/static/media/github.de668f71.png"
+  },
+  {
+    "revision": "f08c524d147f46398cb54013ee7013ee",
+    "url": "/React-portfolio/static/media/linkedin.f08c524d.png"
+  },
+  {
+    "revision": "94935482aeb69791d75ee21dbaf9f383",
+    "url": "/React-portfolio/static/media/mail.94935482.png"
+  },
+  {
+    "revision": "f1f57ff1735efcd4674851a6b84c45bc",
+    "url": "/React-portfolio/static/media/me_grad_better_edited_tree.f1f57ff1.png"
+  },
+  {
+    "revision": "49d1be2183c1cb85445b414469d84b7d",
+    "url": "/React-portfolio/static/media/pizza.49d1be21.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.378a61154e0f3fd3439d0529c83d1c78.js"
+  "/React-portfolio/precache-manifest.073a8e166e7433def47a7e21a28be378.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
